ef598817ccd2acdd18cf2305b29b29d0af5cbc91
Grégor Boirie <gregor.boirie@free.fr>
2023-04-03 11:26:00 +0200

    test: rename test suites

--- test/ebuild.mk
+++ test/ebuild.mk
@@ -10,19 +10,19 @@ test-ldflags := $(common-cflags) \
 bins         :=
 
 ifeq ($(CONFIG_STROLL_BOPS),y)
-bins         += bops
-bops-objs     = bops.o
-bops-cflags   = $(test-cflags)
-bops-ldflags  = $(test-ldflags) -lstroll
-bops-pkgconf  = cmocka
+bins                   += stroll-bops-ut
+stroll-bops-ut-objs     = bops.o
+stroll-bops-ut-cflags   = $(test-cflags)
+stroll-bops-ut-ldflags  = $(test-ldflags) -lstroll
+stroll-bops-ut-pkgconf  = cmocka
 endif # ($(CONFIG_STROLL_BOPS),y)
 
 ifeq ($(CONFIG_STROLL_BMAP),y)
-bins         += bmap
-bmap-objs     = bmap.o
-bmap-cflags   = $(test-cflags)
-bmap-ldflags  = $(test-ldflags) -lstroll
-bmap-pkgconf  = cmocka
+bins                   += stroll-bmap-ut
+stroll-bmap-ut-objs     = bmap.o
+stroll-bmap-ut-cflags   = $(test-cflags)
+stroll-bmap-ut-ldflags  = $(test-ldflags) -lstroll
+stroll-bmap-ut-pkgconf  = cmocka
 endif # ($(CONFIG_STROLL_BMAP),y)
 
 # vim: filetype=make :
